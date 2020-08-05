7f9b2019b4ae312494d4f20eea6f7544b76e6018
jgp <jgp@jgp.net>
2020-08-05 14:46:24 -0400

    More

---
+++ src/main/java/com/advancestores/enterprisecatalog/datafastlane/K.java
@@ -0,0 +1,43 @@
+package com.advancestores.enterprisecatalog.datafastlane;
+
+public abstract class K {
+
+    public static final String SUCCESS = "SUCCESS";
+    public static final String FAILED = "FAILED";
+
+    public static final String STORE_NAME = "$store";
+    protected static final String SPARK = "$spark";
+    protected static final String SCHEMA = "$schema";
+    protected static final String CONNECTION = "$connection";
+    protected static final String IMPORT = "$import";
+
+    // some predefined keys used by SparkContext
+    public static final String KEY_SPARK_EXECUTOR_MEMORY = "spark.executor.memory";
+    public static final String KEY_SPARK_MEMORY_OFFHEAP_ENABLED = "spark.memory.offHeap.enabled";
+    public static final String KEY_SPARK_MEMORY_OFFHEAP_SIZE = "spark.memory.offHeap.size";
+
+    protected static final String APP_NAME_DEFAULT = "Data in the Fast Lane";
+    protected static final String EXECUTOR_MEMORY_DEFAULT = "8g";
+    protected static final String MEMORY_OFF_HEAP_ENABLED_DEFAULT = "true";
+    protected static final String MEMORY_OFF_HEAP_SIZE_DEFAULT = "32g";
+    protected static final String MASTER_DEFAULT = "local[*]";
+
+    public static final String S3_PREFIX = "s3://";
+
+    // AWS option keys
+    public static final String KEY_AWS_ROLE_ARN = "aws_role_arn";
+    public static final String KEY_AWS_REGION = "aws_region";
+    public static final String KEY_AWS_ACCESS_KEY_ID = "aws_access_key_id";
+    public static final String KEY_AWS_SECRET_ACCESS_KEY = "aws_secret_access_key";
+    public static final String KEY_AWS_SESSION_TOKEN = "aws_session_token";
+
+    // general option keys
+    public static final String KEY_SOURCE = "source";
+    public static final String KEY_DESTINATION = "destination";
+    public static final String DISTINCT = "distinct";
+    public static final String NESTING_COLUMN_SUFFIX = "Details";
+    public static final String STRUCT_NAME = "structName";
+    public static final String LIST_NAME_SUFFIX = "s";
+    public static final String LIST_NAME = "listName";
+
+}
---
+++ src/main/java/com/advancestores/enterprisecatalog/datafastlane/context/ReaderContext.java
@@ -0,0 +1,95 @@
+package com.advancestores.enterprisecatalog.datafastlane.context;
+
+import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.advancestores.enterprisecatalog.datafastlane.FastLaneException;
+import com.advancestores.enterprisecatalog.datafastlane.util.Utils;
+import com.google.gson.annotations.Expose;
+
+/**
+ * Stores information surrounding a DataFrameReader. The goal was to use the DataFrameReader options map to store extra
+ * things; however, the property is private. Things can be put in, but not read out.
+ */
+public class ReaderContext {
+    private static final Logger log = LoggerFactory.getLogger(ReaderContext.class);
+
+    private DataFrameReader reader;
+    @Expose()
+    private String filePath;
+    @Expose()
+    private String dataframeName;
+    @Expose()
+    private String connectionName;
+
+    public ReaderContext() {
+    }
+
+    public DataFrameReader getReader() {
+        return reader;
+    }
+
+    public Dataset<Row> load() {
+        Dataset<Row> dataframe = null;
+        long loadStart = System.currentTimeMillis();
+
+        try {
+            if (filePath == null) {
+                log.info(">>>> Loading {} using connection reference {}", dataframeName, connectionName);
+                dataframe = reader.load();
+            }
+            else {
+                log.info(">>>> Loading {} from file {}", dataframeName, filePath);
+                dataframe = reader.load(filePath);
+            }
+
+            log.info(">>>> Load completed in {} seconds", ((System.currentTimeMillis() - loadStart) / 1000));
+        }
+        catch (Exception e) {
+            String errMsg = "An error occurred while loading:\n" + toString() + "\nError: " + e.getMessage();
+            log.error(errMsg);
+            throw new FastLaneException(errMsg);
+        }
+
+        return dataframe;
+    }
+
+    public ReaderContext setReader(DataFrameReader reader) {
+        this.reader = reader;
+        return this;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public ReaderContext setFilePath(String filePath) {
+        this.filePath = filePath;
+        return this;
+    }
+
+    public String getDataframeName() {
+        return dataframeName;
+    }
+
+    public ReaderContext setDataframeName(String dataframeName) {
+        this.dataframeName = dataframeName;
+        return this;
+    }
+
+    public String getConnectionName() {
+        return connectionName;
+    }
+
+    public ReaderContext setConnectionName(String connectionName) {
+        this.connectionName = connectionName;
+        return this;
+    }
+
+    public String toString() {
+        return Utils.getGsonExpose().toJson(this);
+    }
+}
