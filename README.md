bc7441e28d0c5ac729ab9d64033deba8805598e1
Jean-Georges Perrin <jgp@advance-auto.com>
2020-09-08 16:26:03 -0400

    Work on shell and initial doc

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Data in the Fast Lane
+
+
+
+
+## Installation of first execution
+
+Once you have cloned the source code, run:
+
+    mvn install -Dmaven.test.skip=true
+    
+Tests are working but may require external components that are not described in the requirements yet. To run Data in the Fast Lane (DFL), simply run:
+
+    dfl.sh
+
+
---
+++ dfl.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ORIGDIR=`pwd`
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+
+# Check system reqs
+if [[ `type java` ]]; then
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "Java not found ... [fail]"
+    exit -1
+fi
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    if [[ "$version" > "11" ]]; then
+        echo "Java version is above 11 ... [pass]"
+    else         
+        echo Java is too old, requires Java 11, found "$version" [fail]
+        exit -1
+    fi
+fi
+
+echo $*
+pwd
+mvn -q exec:exec -Dexec.workingdir=$BASEDIR -Dexec.executable=_java -DADD_ARGS="$*" -Dlog4j.configuration=file:$BASEDIR/src/main/resources/log4j.properties -Dmaven.test.skip=true
+
+cd $ORIGDIR
